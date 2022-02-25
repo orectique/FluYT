35667ae4059e35c0f6af103d49c9f38c36823e9b
Sreehari Pulickamadhom Sreedhar <49713741+orectique@users.noreply.github.com>
2022-02-25 15:06:40 +0530

    build wheel

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
-import sys
-from cx_Freeze import setup, Executable
-
-# Dependencies are automatically detected, but it might need fine tuning.
-# "packages": ["os"] is used as example only
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
-
-# base="Win32GUI" should be used only for Windows GUI app
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+from setuptools import setup
 
 setup(
-    name = "FluYT",
-    version = "0.1",
-    description = "An elementary YouTube audio downloader",
-    options = {"build_exe": build_exe_options},
-    executables = [Executable("fluyt.py", base=base)]
+    name='fluyt',
+    version='0.0.1',
+    packages=['fluyt'],
+    install_requires=[
+        'requests',
+        'importlib; python_version > "3.5"',
+        'PySide6',
+        'youtube_dl',
+        'shutil'
+    ],
 )
